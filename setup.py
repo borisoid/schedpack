de74165b2070359528b3d0bd6aae64c2d46094fe
borisoid <boris.lazarev90@mail.ru>
2022-10-09 18:05:46 +0300

    setup.py refactor; url update

--- setup.py
+++ setup.py
@@ -11,25 +11,30 @@ README = Path("README.md").read_text()
 
 
 setup(
-    name='schedpack',
-    packages=['schedpack'],
-    version='1.1.1',
-    license='MIT',
-    description='Package for scheduling activities that last some time',
+    name="schedpack",
+
+    packages=["schedpack"],
+    version="1.1.1",
+
+    description="Package for scheduling activities that last some time",
     long_description=README,
-    long_description_content_type='text/markdown',
-    author='borisoid',
-    url='https://github.com/Borisoid/schedpack',
-    keywords=['schedule', 'lasting', 'cron'],
+    long_description_content_type="text/markdown",
+    keywords=["schedule", "lasting", "span", "cron"],
+
+    license="MIT",
+    author="borisoid",
+    url="https://github.com/borisoid/schedpack",
+
     install_requires=[
-        'croniter==1.0.15',
+        "croniter==1.0.15",
     ],
+
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
     ],
 )
